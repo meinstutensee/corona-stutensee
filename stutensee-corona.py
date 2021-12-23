4e57443a374872dff5f78d35fa9daf633655ef9d
Daniel Weisser <daniel.weisser@gmail.com>
2021-12-23 10:07:51 +0100

    Test prediction with prophet

--- stutensee-corona.py
+++ stutensee-corona.py
@@ -4,6 +4,8 @@ import tabula
 from bs4 import BeautifulSoup
 from datetime import datetime
 from pathlib import Path
+from prophet import Prophet
+
 
 # Load CSV
 df = pd.read_csv('daten.csv')
@@ -17,6 +19,7 @@ if last_update[0] > df.iloc[-1]['Datum']:
     print("Need new data.")
     # Get PDF and load it
     pdfUrl = soup.find('a', href=True, text='Fallzahlen in tabellarischer Form (PDF Download)')['href']
+    print("Trying to download: " + pdfUrl)
     t = tabula.read_pdf(pdfUrl, pages=1)
     # Parse data
     df = df.append({'Datum': last_update[0],
@@ -39,6 +42,18 @@ df['7-Tage Inzidenz'] = df['Gesamtzahl'].diff(periods=7) / 25052 * 100000
 ax = df.plot.line(x='Datum', y='Infiziert')
 df.plot.line(x='Datum', y='Neue Infektionen', ax=ax).get_figure().savefig('infektionen.png')
 df.plot.line(x='Datum', y='7-Tage Inzidenz').get_figure().savefig('inzidenz.png')
+
+# Prediction
+m = Prophet()
+df2 = df.rename(columns={'Datum': 'ds', 'Gesamtzahl': 'y'})
+m.fit(df2)
+future = m.make_future_dataframe(periods=90)
+forecast = m.predict(future)
+
+# see https://towardsdatascience.com/simplified-forecasting-with-facebook-prophet-d5789d32acdf
+print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(50))
+m.plot(forecast).savefig('future.png')
+m.plot_components(forecast).savefig('future2.png')
 
 # Write 7-day-incidence
 seven_day = round(df.iloc[-1]["7-Tage Inzidenz"], 1)
